bb4f9777cca8ba69b9a1932593841e6beeb2ab1e
Najla Vahdani <najlavhdni@gmail.com>
2024-07-12 16:28:51 +0330

    option 3

--- GasStation.java
+++ GasStation.java
@@ -12,19 +12,36 @@ public class GasStation {
     private static int providedFuels = 0; //kol fuel erae dade shode
     
 
+    public static void addToLogsFile(String log){
+        //har kodoom az log haro tooye file zakhore mikonim
+        try {
+            FileWriter fileWriter = new FileWriter("./Logs.txt", true);
+            BufferedWriter bufferWriter = new BufferedWriter(fileWriter);
 
-    public static int calcRemaningFuel(FuelType type){
-        switch (type) {
-            case FuelType.REGULAR:
-                return ((GasStation.regPump.getStorage()) + (GasStation.regPump.getFuelPump()));
-            case FuelType.SUPREME:
-                return ((GasStation.supPump.getStorage()) + (GasStation.supPump.getFuelPump()));
-            case FuelType.EURO4:
-                return ((GasStation.Euro4Pump.getStorage()) + (GasStation.Euro4Pump.getFuelPump()));
-            default:
-                return 0;
-        }
+            bufferWriter.write(log + "\n");
+
+            bufferWriter.flush();
+            bufferWriter.close();
+            fileWriter.flush();
+            fileWriter.close();
+
+        } catch (IOException e) {
+           // TODO: handle exception
+        } 
     }
+
+    // public static int calcRemaningFuel(FuelType type){
+    //     switch (type) {
+    //         case FuelType.REGULAR:
+    //             return ((GasStation.regPump.getStorage()) + (GasStation.regPump.getFuelPump()));
+    //         case FuelType.SUPREME:
+    //             return ((GasStation.supPump.getStorage()) + (GasStation.supPump.getFuelPump()));
+    //         case FuelType.EURO4:
+    //             return ((GasStation.Euro4Pump.getStorage()) + (GasStation.Euro4Pump.getFuelPump()));
+    //         default:
+    //             return 0;
+    //     }
+    // }
 
     public static void addToProvidedFuel(int liters){
         GasStation.providedFuels = providedFuels + liters;
@@ -44,8 +61,8 @@ public class GasStation {
             fileWriter.flush();
             fileWriter.close();
 
-        } catch (Exception e) {
-            System.out.println("ey vay badbakht shodim ke:(");
+        } catch (IOException e) {
+            // TODO: handle exception
         } 
     }
 
@@ -61,11 +78,13 @@ public class GasStation {
             int opt = input.nextInt();
             switch (opt){
                 case 1:
+                    String log;
                     System.out.println("Hi! Please Enter The Requested Items:");
                     System.out.println("Number Of The Cars: ");
                     int queueLength = input.nextInt();
                     GasStation.carsQueue = new Car[queueLength];
-                    for(int i=0; i<queueLength; i++){
+                    
+                    for(int i=0; i<queueLength; i++){  //voroodi migire etelaate har mashin ro va mizarateshoon tooye saf 
                         System.out.println("car number " + (i+1) + " :");
                         System.out.println("The Car Type:(A or B or C)");
                         Character carType = input.next().charAt(0);
@@ -107,10 +126,16 @@ public class GasStation {
                     }
                     //ta inja saf mashin ha dakhel gas statione tashkol shod
 
-                    for(int i=0; i<GasStation.carsQueue.length; i++){
+                    for(int i=0; i<GasStation.carsQueue.length; i++){ //mashin haye tooye saf ro bezin mizane
                         Car currentCar = GasStation.carsQueue[i];
-                        System.out.println("car number: " + (i+1));
-                        System.out.println("--car with fuel leve " + currentCar.getCarFuelTank() + " is about to start refueling at " + LocalTime.now() + "time!");
+                        log = "car number: " + (i+1); //shomare mashin ro eelam mikone
+                        System.out.println(log);
+                        GasStation.addToLogsFile(log); //in log ro tooye file save mikone
+
+                        log = "--car with fuel leve " + currentCar.getCarFuelTank() + " is about to start refueling at " + LocalTime.now() + "time!";
+                        System.out.println(log);
+                        GasStation.addToLogsFile(log);
+
                         FuelType cfType = currentCar.getCarFuelType(); //type fuel ke mashin kikhad ke bayad ba type pump match bashe
                         Pump currectPump;
                         boolean isRefueling = false;
@@ -119,39 +144,60 @@ public class GasStation {
                     
                         if(cfType == GasStation.regPump.getPumpType()){ //type fuel mashin regulare, az pump regular fuel migire    
                             try {
-                                System.out.println("-- car with fuel level " + currentCar.getCarFuelTank() + " is starting to refueling at " + LocalTime.now() + " time!");
+                                log = "-- car with fuel level " + currentCar.getCarFuelTank() + " is starting to refueling at " + LocalTime.now() + " time!";
+                                System.out.println(log);
+                                GasStation.addToLogsFile(log); //in log ro tooye file save mikone
+
                                 currectPump = GasStation.regPump;
                                 int liters = 0;
+
                                 liters = currentCar.carFuelWanted(currectPump.getppl()); //meghdar fuel ghabe gereftan tavasote mashin hesab mishe
                                 liters = currectPump.refuelingPump(liters); //meghdar fuel ghabele eraee tavasote pump hesab mishe va az pump kam mishe
                                 currentCar.addToCarFuelTank(liters); //fuel be bak mashin ezafe mishe
                                 int price = currentCar.withdrawFromTheWallet(liters, GasStation.regPump.getppl()); //pool benzin az Wallet mashin kam mishe
                                 GasStation.addToWallet(price); //pool be wallet gas station ezaf mishe
-                                Thread.sleep((liters / 50)*2000);
-                                // gs.sleep(liters); //tavaghof barname baraye benzin zadan
-                                System.out.println("-- this car's fuel level after refueling is " + currentCar.getCarFuelTank() + " at " + LocalTime.now());
+                                Thread.sleep((liters / 50)*2000); //tavaghof barname baraye benzin zadan
+                                
+                                log = "-- this car's fuel level after refueling is " + currentCar.getCarFuelTank() + " at " + LocalTime.now();
+                                System.out.println(log);
+                                GasStation.addToLogsFile(log); //in log ro tooye file save mikone
+                                
                                 GasStation.addToProvidedFuel(liters); //meghdar liter dade shode tooye pump benzin ezafe mishe
                                 isRefueling = true;
-                                System.out.println("-- This car left the gas station");
-                                
+
+
+                                log = "-- This car left the gas station";
+                                System.out.println(log);
+                                GasStation.addToLogsFile(log); //in log ro tooye file save mikone
 
                             }catch(NotEnoughMoneyException neme) {
-                                System.out.println("-- this car doesn't have enough money!");
+                                log = "-- this car doesn't have enough money!";
+                                System.out.println(log);
+                                GasStation.addToLogsFile(log); //in log ro tooye file save mikone
+
                                 isRefueling = true; //age pool nadashte bashe niyazi nist baghiye pumpha barash check beshan va log monaseb nist ro badan
                                 
                             }catch(PumpIsEmptyException piee){
-                                System.out.println("-- this pump is empty, go to another gas station, bye!");
+                                log = "-- this pump is empty, go to another gas station, bye!";
+                                System.out.println(log);
+                                GasStation.addToLogsFile(log); //in log ro tooye file save mikone
+
                                 isRefueling = true; //age pump monasebesh fuel nadashte bashe niyazi nist baghiye pumpha barash check beshan va log monaseb nist ro badan
                             }
                         }else if(cfType != GasStation.regPump.getPumpType()){ //type regular baraye mashin match nist
                             if(isRefueling == false){ //hanooz pump monasebesh ro peyda nakarde
-                                System.out.println("-- this station is not compatible to this car at " + LocalTime.now()+ " time!");
+                                log = "-- this station is not compatible to this car at " + LocalTime.now()+ " time!";
+                                System.out.println(log);
+                                GasStation.addToLogsFile(log); //in log ro tooye file save mikone
                             }
                         }
 
                         if(cfType == GasStation.supPump.getPumpType()){
                             try {
-                                System.out.println("-- car with fuel level " + currentCar.getCarFuelTank() + " is starting to refueling at " + LocalTime.now() + " time!");
+                                log = "-- car with fuel level " + currentCar.getCarFuelTank() + " is starting to refueling at " + LocalTime.now() + " time!";
+                                System.out.println(log);
+                                GasStation.addToLogsFile(log); //in log ro tooye file save mikone
+
                                 currectPump = GasStation.regPump;
                                 int liters = 0;
                                 liters = currentCar.carFuelWanted(currectPump.getppl()); //meghdar fuel ghabe gereftan tavasote mashin hesab mishe
@@ -161,29 +207,43 @@ public class GasStation {
                                 GasStation.addToWallet(price); //pool be wallet gas station ezaf mishe
                                 Thread.sleep((liters / 50)*2000);
                                 // gs.sleep(liters); //tavaghof barname baraye benzin zadan
-                                System.out.println("-- this car's fuel level after refueling is " + currentCar.getCarFuelTank() + " at " + LocalTime.now());
+                                log = "-- this car's fuel level after refueling is " + currentCar.getCarFuelTank() + " at " + LocalTime.now();
+                                System.out.println(log);
+                                GasStation.addToLogsFile(log); //in log ro tooye file save mikone
+                                
                                 GasStation.addToProvidedFuel(liters); //meghdar liter dade shode tooye pump benzin ezafe mishe
                                 isRefueling = true;
-                                System.out.println("-- This car left the gas station");
+                                log = "-- This car left the gas station";
+                                System.out.println(log);
+                                GasStation.addToLogsFile(log); //in log ro tooye file save mikone
                                 
 
                             }catch(NotEnoughMoneyException neme) {
-                                System.out.println("-- this car doesn't have enough money!");
+                                log = "-- this car doesn't have enough money!";
+                                System.out.println(log);
+                                GasStation.addToLogsFile(log); //in log ro tooye file save mikone
                                 isRefueling = true;
                                 
                             }catch(PumpIsEmptyException piee){
-                                System.out.println("-- this pump is empty, go ro another gas station, bye!");
+                                log = "-- this pump is empty, go ro another gas station, bye!";
+                                System.out.println(log);
+                                GasStation.addToLogsFile(log); //in log ro tooye file save mikone
                                 isRefueling = true;
                             }
                         }else if(cfType != GasStation.supPump.getPumpType()){
                             if(isRefueling == false){
-                                System.out.println("-- this station is not compatible to this car at " + LocalTime.now()+ " time!");
+                                log = "-- this station is not compatible to this car at " + LocalTime.now()+ " time!";
+                                System.out.println(log);
+                                GasStation.addToLogsFile(log); //in log ro tooye file save mikone
                             }
                         }
 
                         if(cfType == GasStation.Euro4Pump.getPumpType()){
                             try {
-                                System.out.println("-- car with fuel level " + currentCar.getCarFuelTank() + " is starting to refueling at " + LocalTime.now() + " time!");
+                                log = "-- car with fuel level " + currentCar.getCarFuelTank() + " is starting to refueling at " + LocalTime.now() + " time!";
+                                System.out.println(log);
+                                GasStation.addToLogsFile(log); //in log ro tooye file save mikone
+
                                 currectPump = GasStation.regPump;
                                 int liters = 0;
                                 liters = currentCar.carFuelWanted(currectPump.getppl()); //meghdar fuel ghabe gereftan tavasote mashin hesab mishe
@@ -193,22 +253,33 @@ public class GasStation {
                                 GasStation.addToWallet(price); //pool be wallet gas station ezaf mishe
                                 Thread.sleep((liters/50)*2000);
                                 // gs.sleep(liters); //tavaghof barname baraye benzin zadan
-                                System.out.println("-- this car's fuel level after refueling is " + currentCar.getCarFuelTank() + " at " + LocalTime.now());
+                                log = "-- this car's fuel level after refueling is " + currentCar.getCarFuelTank() + " at " + LocalTime.now() + " time!";
+                                System.out.println(log);
+                                GasStation.addToLogsFile(log); //in log ro tooye file save mikone
+
                                 GasStation.addToProvidedFuel(liters); //meghdar liter dade shode tooye pump benzin ezafe mishe
                                 isRefueling = true;
-                                System.out.println("-- This car left the gas station");
+                                log = "-- This car left the gas station";
+                                System.out.println(log);
+                                GasStation.addToLogsFile(log); //in log ro tooye file save mikone
 
                             }catch(NotEnoughMoneyException neme) {
-                                System.out.println("-- this car doesn't have enough money!");
+                                log = "-- this car doesn't have enough money!";
+                                System.out.println(log);
+                                GasStation.addToLogsFile(log); //in log ro tooye file save mikone
                                 isRefueling = true;
                                 
                             }catch(PumpIsEmptyException piee){
-                                System.out.println("-- this pump is empty, go ro another gas station, bye!");
+                                log = "-- this pump is empty, go ro another gas station, bye!";
+                                System.out.println(log);
+                                GasStation.addToLogsFile(log); //in log ro tooye file save mikone
                                 isRefueling = true;
                             }
                         }else if(cfType != GasStation.Euro4Pump.getPumpType()){
                             if(isRefueling == false){
-                                System.out.println("-- this station is not compatible to this car at " + LocalTime.now()+ " time!");
+                                log = "-- this station is not compatible to this car at " + LocalTime.now()+ " time!";
+                                System.out.println(log);
+                                GasStation.addToLogsFile(log); //in log ro tooye file save mikone
                             }
                         }
                         
@@ -219,6 +290,7 @@ public class GasStation {
                     break;
 
                 case 2: //report mide
+                    System.out.println("neshoon nemide!");
                     BufferedReader bufferedReader;
                     FileReader fileReader;
                     //file accounting marboot be hesabdari ro mikhoone o to console print mikone
@@ -231,11 +303,28 @@ public class GasStation {
                         } 
                         bufferedReader.close();
                         fileReader.close();                        
-                    } catch (Exception e) {
-                        System.out.println("ey vay badbakht shodim ke:(");
+                    } catch (IOException e) {
+                        // TODO: handle exception
                     }
 
-                case 3:
+                    break;
+
+                case 3: //file log haro mikhoone o to console print mikone
+                    try {
+                        fileReader = new FileReader("./Logs.txt");
+                        bufferedReader = new BufferedReader(fileReader);
+                        String line;
+                        while(( line = bufferedReader.readLine()) != null){
+                            System.out.println(line);
+                        } 
+                        bufferedReader.close();
+                        fileReader.close();                        
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+                    break;
+
+
                 default:
                     break;
             }
